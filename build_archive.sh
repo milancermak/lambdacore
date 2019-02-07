30b24ad07ea0fbbf4fbce96d1419ed30e227ae09
Milan Cermak <milan.cermak@gmail.com>
2019-02-07 21:31:07 +0100

    Tweaking .sh scripts [skip ci]

--- build_archive.sh
+++ build_archive.sh
@@ -5,6 +5,14 @@ set -o pipefail
 set -o nounset
 #set -o xtrace
 
+if [ -e layer.zip ]; then
+    rm layer.zip
+fi
+
+if [ -d python ]; then
+    rm -r ./python
+fi
+
 # the resulting library (layer) needs to be in a directory named
 # python, so that when it is extracted in the Lambda exec env,
 # into /opt, is can be easily import-ed; see:
--- publish_layer.sh
+++ publish_layer.sh
@@ -6,7 +6,7 @@ set -o nounset
 #set -o xtrace
 
 aws lambda publish-layer-version \
---layer-name py-core \
+--layer-name lambdacore \
 --compatible-runtimes python3.6 python3.7 \
 --license-info Apache-2.0 \
 --zip-file fileb://layer.zip
